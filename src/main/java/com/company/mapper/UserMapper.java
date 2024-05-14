05bbfc036c974668f41ee019add406a681d880e1
Maria <kitmaria.2015@gmail.com>
2024-05-14 14:51:19 -0500

    created UserMapper

---
+++ src/main/java/com/company/mapper/UserMapper.java
@@ -0,0 +1,23 @@
+package com.company.mapper;
+
+import com.company.dto.RoleDTO;
+import com.company.dto.UserDTO;
+import com.company.entity.Role;
+import com.company.entity.User;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+    private final ModelMapper modelMapper;
+
+    public UserMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+    public User convertToEntity(UserDTO userDTO){
+        return modelMapper.map(userDTO,User.class);
+    }
+    public UserDTO convertToDTO(User entity){
+        return modelMapper.map(entity,UserDTO.class);
+    }
+}
