13f97c801d5ae10421d9bc1ef695afe4434b393c
Maria <kitmaria.2015@gmail.com>
2024-05-14 11:57:30 -0500

    created methods to convert entity and dto objects

---
+++ src/main/java/com/company/mapper/RoleMapper.java
@@ -0,0 +1,22 @@
+package com.company.mapper;
+
+import com.company.dto.RoleDTO;
+import com.company.entity.Role;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RoleMapper {
+    private final ModelMapper modelMapper;
+
+    public RoleMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    public Role convertToEntity(RoleDTO roleDTO){
+        return modelMapper.map(roleDTO,Role.class);
+    }
+    public RoleDTO convertToDTO(Role entity){
+        return modelMapper.map(entity,RoleDTO.class);
+    }
+}
