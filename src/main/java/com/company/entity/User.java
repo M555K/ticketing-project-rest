4fd17619858a09b1103ff66fd49b2d7aa9057f75
Maria <kitmaria.2015@gmail.com>
2024-05-14 10:44:10 -0500

    completed User entity

--- src/main/java/com/company/entity/User.java
+++ src/main/java/com/company/entity/User.java
@@ -3,11 +3,17 @@ package com.company.entity;
 import com.company.entity.Role;
 import com.company.enums.Gender;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Setter
+@Entity
+@Table(name ="users")
 @NoArgsConstructor
 public class User extends BaseEntity {
 
@@ -17,19 +23,10 @@ public class User extends BaseEntity {
     private String passWord;
     private boolean enabled;
     private String phone;
+    @ManyToOne
     private Role role;
+    @Enumerated(EnumType.STRING)
     private Gender gender;
 
-    public User(Long id, LocalDateTime insertDateTime, Long insertUserId, LocalDateTime lastUpdateDateTime, Long lastUpdateUserId, String firstName, String lastName, String userName, String passWord, boolean enabled, String phone, Role role, Gender gender) {
-        super(id, insertDateTime, insertUserId, lastUpdateDateTime, lastUpdateUserId);
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.userName = userName;
-        this.passWord = passWord;
-        this.enabled = enabled;
-        this.phone = phone;
-        this.role = role;
-        this.gender = gender;
-    }
 
 }
