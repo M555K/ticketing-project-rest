2514523a00a69b3e74a2f27659c5c5f279f2dddc
Maria <kitmaria.2015@gmail.com>
2024-05-15 11:11:39 -0500

    implemented findById()

--- src/main/java/com/company/service/impl/RoleServiceImpl.java
+++ src/main/java/com/company/service/impl/RoleServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -43,6 +44,8 @@ public class RoleServiceImpl implements RoleService {
 
     @Override
     public RoleDTO findById(Long id) {
-        return null;
+//       Role role = roleRepository.findById(id).get();
+//        return roleMapper.convertToDTO(role);
+      return roleMapper.convertToDTO(roleRepository.findById(id).get());
     }
 }
