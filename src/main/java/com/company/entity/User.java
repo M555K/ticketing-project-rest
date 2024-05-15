0762addeda8fc67a65e7e94614b4ea3f67d21749
Maria <kitmaria.2015@gmail.com>
2024-05-15 16:00:22 -0500

    added @Where in the User entity to not see the user that is deleted

--- src/main/java/com/company/entity/User.java
+++ src/main/java/com/company/entity/User.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -15,6 +16,7 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name ="users")
 @NoArgsConstructor
+@Where(clause = "is_deleted=false")
 public class User extends BaseEntity {
 
     private String firstName;
