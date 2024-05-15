19ef92183c78eb42c57d8e35d8d38a646c6f2435
Maria <kitmaria.2015@gmail.com>
2024-05-15 11:07:16 -0500

    implemented findByUserName()

--- src/main/java/com/company/service/impl/UserServiceImpl.java
+++ src/main/java/com/company/service/impl/UserServiceImpl.java
@@ -2,11 +2,9 @@ package com.company.service.impl;
 
 import com.company.dto.UserDTO;
 import com.company.entity.User;
-import com.company.mapper.RoleMapper;
 import com.company.mapper.UserMapper;
 import com.company.repository.UserRepository;
 import com.company.service.UserService;
-import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +15,6 @@ public class UserServiceImpl implements UserService {
     private final UserMapper userMapper;
     public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
         this.userRepository = userRepository;
-
         this.userMapper = userMapper;
     }
 
@@ -25,17 +22,16 @@ public class UserServiceImpl implements UserService {
     public List<UserDTO> listAllUsers() {
         List<User> usersList =userRepository.findAll(Sort.by("firstName"));
         return usersList.stream().map(userMapper::convertToDTO).toList();
-
     }
 
     @Override
     public UserDTO findByUserName(String username) {
-        return null;
+       return userMapper.convertToDTO(userRepository.findByUserName(username));
     }
 
     @Override
     public void save(UserDTO user) {
-
+        userRepository.save(userMapper.convertToEntity(user));
     }
 
     @Override
