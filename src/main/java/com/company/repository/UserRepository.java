447b656c59f4bb3b88f35e10383de94c52643782
Maria <kitmaria.2015@gmail.com>
2024-05-16 14:37:28 -0500

    added findByRoleDescriptionIgnoreCase()

--- src/main/java/com/company/repository/UserRepository.java
+++ src/main/java/com/company/repository/UserRepository.java
@@ -3,7 +3,10 @@ package com.company.repository;
 import com.company.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User,Long> {
       User findByUserName(String username);
       void deleteByUserName(String username);
+      List<User> findByRoleDescriptionIgnoreCase(String role);
 }
