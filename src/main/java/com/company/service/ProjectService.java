36eb78eeae9722037ff99d22446f63a7d087f322
Maria <kitmaria.2015@gmail.com>
2024-05-16 14:00:54 -0500

    ProjectService is created

---
+++ src/main/java/com/company/service/ProjectService.java
@@ -0,0 +1,13 @@
+package com.company.service;
+
+import com.company.dto.ProjectDTO;
+
+import java.util.List;
+
+public interface ProjectService {
+    ProjectDTO getByProjectCode(String code);
+    List<ProjectDTO> listAllProjects();
+    void save(ProjectDTO projectDTO);
+    void update(ProjectDTO projectDTO);
+    void delete(ProjectDTO projectDTO);
+}
