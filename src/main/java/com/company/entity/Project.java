988a646fb73e82cdff1083099f31440d7c1caf0f
Maria <kitmaria.2015@gmail.com>
2024-05-20 14:38:53 -0500

    added constraint for projectCode

--- src/main/java/com/company/entity/Project.java
+++ src/main/java/com/company/entity/Project.java
@@ -17,7 +17,7 @@ import java.time.LocalDate;
 @Where(clause = "is_deleted=false")
 public class Project extends BaseEntity{
     private String projectCode;
-
+    @Column(unique = true)//in UI only projectCode should be unique, but DB doesn't know about it, when i created project with the same projectCode it created
     private String projectName;
     @Column(columnDefinition = "DATE")
     private LocalDate startDate;
