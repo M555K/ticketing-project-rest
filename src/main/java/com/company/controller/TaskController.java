573462ea66d81cb9725a4654983cb5c6e1a2f8c0
Maria <kitmaria.2015@gmail.com>
2024-05-20 15:22:45 -0500

    implemented insertTask endpoint

--- src/main/java/com/company/controller/TaskController.java
+++ src/main/java/com/company/controller/TaskController.java
@@ -6,7 +6,10 @@ import com.company.service.ProjectService;
 import com.company.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/task")
@@ -34,25 +37,25 @@ public class TaskController {
 
     }
 
-//    @PostMapping("/create")
-//    public String insertTask(@Valid @ModelAttribute("task") TaskDTO task, BindingResult bindingResult, Model model) {
-//
-//        if (bindingResult.hasErrors()) {
-//
-//            model.addAttribute("projects", projectService.findAll());
-//            model.addAttribute("employees", userService.findEmployees());
-//            model.addAttribute("tasks", taskService.findAll());
-//
-//            return "/task/create";
-//
-//        }
-//
-//        taskService.save(task);
-//
-//        return "redirect:/task/create";
-//
-//    }
-//
+    @PostMapping("/create")
+    public String insertTask( @ModelAttribute("task") TaskDTO task, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+
+            model.addAttribute("projects", projectService.listAllProjects());
+            model.addAttribute("employees", userService.listAllByRole("employee"));
+            model.addAttribute("tasks", taskService.listAllTasks());
+
+            return "/task/create";
+
+        }
+
+        taskService.save(task);
+
+        return "redirect:/task/create";
+
+    }
+
 //    @GetMapping("/delete/{id}")
 //    public String deleteTask(@PathVariable("id") Long id) {
 //        taskService.deleteById(id);
