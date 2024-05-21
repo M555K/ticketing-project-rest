6a96e1522f8826a088e0aa1d9f3753530e70a9ca
Maria <kitmaria.2015@gmail.com>
2024-05-21 15:15:19 -0500

    added findAllByAssignedManager

--- src/main/java/com/company/repository/ProjectRepository.java
+++ src/main/java/com/company/repository/ProjectRepository.java
@@ -1,10 +1,14 @@
 package com.company.repository;
 
 import com.company.entity.Project;
+import com.company.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface ProjectRepository extends JpaRepository<Project,Long> {
     Project findByProjectCode(String code);
+    List<Project> findAllByAssignedManager(User manager);
 }
