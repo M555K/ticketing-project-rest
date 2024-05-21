f75c58d442d969a67e072e0cab90aa195cd7132c
Maria <kitmaria.2015@gmail.com>
2024-05-21 15:26:16 -0500

    implemented listAllProjectDetails()

--- src/main/java/com/company/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/company/service/impl/ProjectServiceImpl.java
@@ -1,25 +1,37 @@
 package com.company.service.impl;
 
 import com.company.dto.ProjectDTO;
+import com.company.dto.UserDTO;
 import com.company.entity.Project;
+import com.company.entity.User;
 import com.company.enums.Status;
 import com.company.mapper.ProjectMapper;
+import com.company.mapper.UserMapper;
 import com.company.repository.ProjectRepository;
 import com.company.service.ProjectService;
+import com.company.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ProjectServiceImpl implements ProjectService {
-private final ProjectRepository projectRepository;
+    private static final Logger log = LoggerFactory.getLogger(ProjectServiceImpl.class);
+    private final ProjectRepository projectRepository;
 private final ProjectMapper projectMapper;
+private final UserService userService;
+private  final UserMapper userMapper;
 
-    public ProjectServiceImpl(ProjectRepository projectRepository, ProjectMapper projectMapper) {
+    public ProjectServiceImpl(ProjectRepository projectRepository, ProjectMapper projectMapper, UserService userService, UserMapper userMapper) {
         this.projectRepository = projectRepository;
         this.projectMapper = projectMapper;
+        this.userService = userService;
+        this.userMapper = userMapper;
     }
 
     @Override
@@ -65,4 +77,20 @@ private final ProjectMapper projectMapper;
         projectRepository.save(projectDTO);
     }
 
+    @Override
+    public List<ProjectDTO> listAllProjectDetails() {
+        UserDTO currentUser = userService.findByUserName("harold@manager.com");
+        User user = userMapper.convertToEntity(currentUser);
+        // get all project with specific manager
+        List<Project> list = projectRepository.findAllByAssignedManager(user);
+        // I need to set completed and pending task
+        //so first convert to the dto and then set the fields
+     return list.stream().map(project ->{
+            ProjectDTO projectDTO = projectMapper.convertToDTO(project);
+            projectDTO.setCompleteTaskCounts(3);
+            projectDTO.setUnfinishedTaskCounts(2);
+            return projectDTO;
+        }).collect(Collectors.toList());
+
+    }
 }
