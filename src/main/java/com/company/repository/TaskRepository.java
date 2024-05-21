5734761e21be8bba6b2e20bda993e6f94d314320
Maria <kitmaria.2015@gmail.com>
2024-05-21 15:52:50 -0500

    implemented  methods for counting the completed and pending tasks

--- src/main/java/com/company/repository/TaskRepository.java
+++ src/main/java/com/company/repository/TaskRepository.java
@@ -2,7 +2,12 @@ package com.company.repository;
 
 import com.company.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TaskRepository extends JpaRepository<Task,Long> {
-
+    @Query ("select count(t) from Task t where t.project.projectCode = ?1 and t.project.projectStatus= 'IN_PROGRESS' or t.project.projectStatus='OPEN'")
+   // @Query ("select count(t) from Task t where t.project.projectCode = ?1 and t.project.projectStatus <> 'COMPLETE'")
+    int totalNonCompletedTasks(String projectCode);
+    @Query (value = "select count(*) from tasks t join projects p on t.project_id = p.id where p.project_code = ?1 and p.status_code = 'COMPLETE'",nativeQuery = true)
+    int totalCompletedTasks(String projectCode);
 }
