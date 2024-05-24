0c0c986d2816012ffea9c2192de1ff5fa7f7f2a8
Maria <kitmaria.2015@gmail.com>
2024-05-24 12:38:54 -0500

    changes

--- src/main/java/com/company/repository/TaskRepository.java
+++ src/main/java/com/company/repository/TaskRepository.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task,Long> {
     @Query ("select count(t) from Task t where t.project.projectCode = ?1 and t.taskStatus = 'IN_PROGRESS' or t.project.projectCode = ?1 and t.taskStatus = 'OPEN'")
-   // @Query ("select count(t) from Task t where t.project.projectCode = ?1 and t.project.projectStatus <> 'COMPLETE'")
+   // @Query ("select count(t) from Task t where t.project.projectCode = ?1 and t.taskStatus <> 'COMPLETE'")
     int totalNonCompletedTasks(String projectCode);
     @Query (value = "select count(*) from tasks t join projects p on t.project_id = p.id where p.project_code = ?1 and t.task_status = 'COMPLETE'",nativeQuery = true)
     int totalCompletedTasks(String projectCode);
