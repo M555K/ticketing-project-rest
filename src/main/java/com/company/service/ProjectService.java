20252a0e20b5bbd2e5493a19a5686f957ac51fea
Maria <kitmaria.2015@gmail.com>
2024-05-24 12:58:55 -0500

    added listAllNonCompletedByAssignedManager(UserDTO assignedManager)

--- src/main/java/com/company/service/ProjectService.java
+++ src/main/java/com/company/service/ProjectService.java
@@ -1,6 +1,7 @@
 package com.company.service;
 
 import com.company.dto.ProjectDTO;
+import com.company.dto.UserDTO;
 
 import java.util.List;
 
@@ -11,4 +12,8 @@ public interface ProjectService {
     void update(ProjectDTO projectDTO);
     void delete(String projectDTO);
     void complete(String projectCode);
+
+    List<ProjectDTO> listAllProjectDetails();
+
+    List<ProjectDTO> listAllNonCompletedByAssignedManager(UserDTO assignedManager);
 }
