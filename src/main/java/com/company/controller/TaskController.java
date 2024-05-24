4e065e2197090997c0c8d01c9546705905903e35
Maria <kitmaria.2015@gmail.com>
2024-05-24 11:13:41 -0500

    completed archive and pending task endpoints

--- src/main/java/com/company/controller/TaskController.java
+++ src/main/java/com/company/controller/TaskController.java
@@ -1,15 +1,14 @@
 package com.company.controller;
 
 import com.company.dto.TaskDTO;
-import com.company.repository.TaskService;
+import com.company.enums.Status;
+import com.company.service.TaskService;
 import com.company.service.ProjectService;
 import com.company.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/task")
@@ -56,24 +55,24 @@ public class TaskController {
 
     }
 
-//    @GetMapping("/delete/{id}")
-//    public String deleteTask(@PathVariable("id") Long id) {
-//        taskService.deleteById(id);
-//        return "redirect:/task/create";
-//    }
-//
-//    @GetMapping("/update/{taskId}")
-//    public String editTask(@PathVariable("taskId") Long taskId, Model model) {
-//
-//        model.addAttribute("task", taskService.findById(taskId));
-//        model.addAttribute("projects", projectService.findAll());
-//        model.addAttribute("employees", userService.findEmployees());
-//        model.addAttribute("tasks", taskService.findAll());
-//
-//        return "/task/update";
-//
-//    }
-//
+    @GetMapping("/delete/{id}")
+    public String deleteTask(@PathVariable("id") Long id) {
+        taskService.delete(id);
+        return "redirect:/task/create";
+    }
+
+    @GetMapping("/update/{taskId}")
+    public String editTask(@PathVariable("taskId") Long taskId, Model model) {
+
+        model.addAttribute("task", taskService.getById(taskId));
+        model.addAttribute("projects", projectService.listAllProjects());
+        model.addAttribute("employees", userService.listAllByRole("employee"));
+        model.addAttribute("tasks", taskService.listAllTasks());
+
+        return "/task/update";
+
+    }
+
 ////    @PostMapping("/update/{taskId}")
 ////    public String updateTask(@PathVariable("taskId") Long taskId, TaskDTO task) {
 ////
@@ -84,37 +83,37 @@ public class TaskController {
 ////
 ////    }
 //
-//    @PostMapping("/update/{id}")
-//    public String updateTask(@Valid @ModelAttribute("task") TaskDTO task, BindingResult bindingResult, Model model) {
-//
-//        if (bindingResult.hasErrors()) {
-//
-//            model.addAttribute("projects", projectService.findAll());
-//            model.addAttribute("employees", userService.findEmployees());
-//            model.addAttribute("tasks", taskService.findAll());
-//
-//            return "/task/update";
-//
-//        }
-//
-//        taskService.update(task);
-//
-//        return "redirect:/task/create";
-//
-//    }
-//
-//    @GetMapping("/employee/pending-tasks")
-//    public String employeePendingTasks(Model model) {
-//        model.addAttribute("tasks", taskService.findAllTasksByStatusIsNot(Status.COMPLETE));
-//        return "/task/pending-tasks";
-//    }
-//
-//    @GetMapping("/employee/archive")
-//    public String employeeArchivedTasks(Model model) {
-//        model.addAttribute("tasks", taskService.findAllTasksByStatus(Status.COMPLETE));
-//        return "/task/archive";
-//    }
-//
+    @PostMapping("/update/{id}")
+    public String updateTask( @ModelAttribute("task") TaskDTO task, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+
+            model.addAttribute("projects", projectService.listAllProjects());
+            model.addAttribute("employees", userService.listAllByRole("employee"));
+            model.addAttribute("tasks", taskService.listAllTasks());
+
+            return "/task/update";
+
+        }
+
+        taskService.update(task);
+
+        return "redirect:/task/create";
+
+    }
+
+    @GetMapping("/employee/pending-tasks")
+    public String employeePendingTasks(Model model) {
+        model.addAttribute("tasks", taskService.listAllTasksByStatusIsNot(Status.COMPLETE));
+        return "/task/pending-tasks";
+    }
+
+    @GetMapping("/employee/archive")
+    public String employeeArchivedTasks(Model model) {
+        model.addAttribute("tasks", taskService.listAllTasksByStatus(Status.COMPLETE));
+        return "/task/archive";
+    }
+
 //    @GetMapping("/employee/edit/{id}")
 //    public String employeeEditTask(@PathVariable Long id, Model model) {
 //
