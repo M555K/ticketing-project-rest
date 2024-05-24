f936b47a43e5b97f54c3b65d82f9638cbf9fa2bf
Maria <kitmaria.2015@gmail.com>
2024-05-24 08:49:49 -0500

    added findAllByProject() for task deletion

--- src/main/java/com/company/repository/TaskRepository.java
+++ src/main/java/com/company/repository/TaskRepository.java
@@ -1,8 +1,11 @@
 package com.company.repository;
 
+import com.company.entity.Project;
 import com.company.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task,Long> {
     @Query ("select count(t) from Task t where t.project.projectCode = ?1 and t.taskStatus = 'IN_PROGRESS' or t.project.projectCode = ?1 and t.taskStatus = 'OPEN'")
@@ -10,4 +13,5 @@ public interface TaskRepository extends JpaRepository<Task,Long> {
     int totalNonCompletedTasks(String projectCode);
     @Query (value = "select count(*) from tasks t join projects p on t.project_id = p.id where p.project_code = ?1 and t.task_status = 'COMPLETE'",nativeQuery = true)
     int totalCompletedTasks(String projectCode);
+    List<Task> findAllByProject(Project project);
 }
