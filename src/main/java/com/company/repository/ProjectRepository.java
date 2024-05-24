94e9b2caf3e1134098a18cfc49ec9499099f3dac
Maria <kitmaria.2015@gmail.com>
2024-05-24 12:58:23 -0500

    added findAllByProjectStatusIsNotAndAssignedManager(Status status, User assignedManager)

--- src/main/java/com/company/repository/ProjectRepository.java
+++ src/main/java/com/company/repository/ProjectRepository.java
@@ -2,6 +2,7 @@ package com.company.repository;
 
 import com.company.entity.Project;
 import com.company.entity.User;
+import com.company.enums.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
@@ -11,4 +12,6 @@ import java.util.List;
 public interface ProjectRepository extends JpaRepository<Project,Long> {
     Project findByProjectCode(String code);
     List<Project> findAllByAssignedManager(User manager);
+
+    List<Project> findAllByProjectStatusIsNotAndAssignedManager(Status status, User assignedManager);
 }
