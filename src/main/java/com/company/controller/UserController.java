e360b125215838fa067ef280f77cb43c8b43cf86
Maria <kitmaria.2015@gmail.com>
2024-07-02 16:19:17 -0500

    added restriction for user endpoints

--- src/main/java/com/company/controller/UserController.java
+++ src/main/java/com/company/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
 import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,27 +24,32 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
+    @RolesAllowed({"Manager","Admin"})
     @GetMapping
     public ResponseEntity<ResponseWrapper> getUsers(){
         List<UserDTO> userDTOList = userService.listAllUsers();
         return ResponseEntity.ok(new ResponseWrapper("Users are successfully retrieved",userDTOList, HttpStatus.OK));
     }
+    @RolesAllowed("Admin")
     @GetMapping("/{username}")
     public ResponseEntity<ResponseWrapper> getUserByUserName(@PathVariable("username") String userName){
         UserDTO user = userService.findByUserName(userName);
         return ResponseEntity.ok(new ResponseWrapper("User is successfully retrieved",user, HttpStatus.OK));
     }
+    @RolesAllowed("Admin")
     @PostMapping
     public ResponseEntity<ResponseWrapper> createUser(@RequestBody UserDTO user){
         userService.save(user);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(new ResponseWrapper("Users are successfully created",user, HttpStatus.CREATED));
     }
+    @RolesAllowed("Admin")
     @PutMapping
     public ResponseEntity<ResponseWrapper> updateUser(@RequestBody UserDTO user){
         userService.update(user);
         return ResponseEntity.ok(new ResponseWrapper("User are successfully updated",user, HttpStatus.OK));
     }
+    @RolesAllowed("Admin")
     @DeleteMapping("/{username}")
     public ResponseEntity<ResponseWrapper> deleteUser(@PathVariable("username") String userName){
         userService.delete(userName);
