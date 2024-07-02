be3d3bf880fa0419fd206e247211f70ed5e6da9a
Maria <kitmaria.2015@gmail.com>
2024-07-02 12:49:48 -0500

    Rename class

--- src/main/java/com/company/TicketingProjectRestApplication.java
+++ src/main/java/com/company/TicketingProjectRestApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class TicketingProjectDataApplication {
+public class TicketingProjectRestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TicketingProjectDataApplication.class, args);
+		SpringApplication.run(TicketingProjectRestApplication.class, args);
 	}
    @Bean
 	public ModelMapper mapper(){
