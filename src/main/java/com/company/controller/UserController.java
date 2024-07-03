f9b95203a1513c4cb53403cb842b7f240459ab05
Maria <kitmaria.2015@gmail.com>
2024-07-03 15:50:50 -0500

    added swagger annotations

--- src/main/java/com/company/controller/UserController.java
+++ src/main/java/com/company/controller/UserController.java
@@ -4,18 +4,14 @@ import com.company.dto.ResponseWrapper;
 import com.company.dto.UserDTO;
 import com.company.service.RoleService;
 import com.company.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
 import javax.annotation.security.RolesAllowed;
-import javax.validation.Valid;
-import java.util.ArrayList;
 import java.util.List;
-
+@Tag(name = "UserController", description = "User API")
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
@@ -26,18 +22,21 @@ public class UserController {
     }
     @RolesAllowed({"Manager","Admin"})
     @GetMapping
+    @Operation(summary = "Get users")
     public ResponseEntity<ResponseWrapper> getUsers(){
         List<UserDTO> userDTOList = userService.listAllUsers();
         return ResponseEntity.ok(new ResponseWrapper("Users are successfully retrieved",userDTOList, HttpStatus.OK));
     }
     @RolesAllowed("Admin")
     @GetMapping("/{username}")
+    @Operation(summary = "Get users by username")
     public ResponseEntity<ResponseWrapper> getUserByUserName(@PathVariable("username") String userName){
         UserDTO user = userService.findByUserName(userName);
         return ResponseEntity.ok(new ResponseWrapper("User is successfully retrieved",user, HttpStatus.OK));
     }
     @RolesAllowed("Admin")
     @PostMapping
+    @Operation(summary = "Create user")
     public ResponseEntity<ResponseWrapper> createUser(@RequestBody UserDTO user){
         userService.save(user);
         return ResponseEntity.status(HttpStatus.CREATED)
@@ -45,12 +44,14 @@ public class UserController {
     }
     @RolesAllowed("Admin")
     @PutMapping
+    @Operation(summary = "Updare user")
     public ResponseEntity<ResponseWrapper> updateUser(@RequestBody UserDTO user){
         userService.update(user);
         return ResponseEntity.ok(new ResponseWrapper("User are successfully updated",user, HttpStatus.OK));
     }
     @RolesAllowed("Admin")
     @DeleteMapping("/{username}")
+    @Operation(summary = "Delete user")
     public ResponseEntity<ResponseWrapper> deleteUser(@PathVariable("username") String userName){
         userService.delete(userName);
         return ResponseEntity.status(HttpStatus.NO_CONTENT)
