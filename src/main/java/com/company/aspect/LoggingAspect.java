4c48f7180a344c8b8385ba57a2a4df2eab0cb563
Maria <kitmaria.2015@gmail.com>
2024-07-09 11:51:05 -0500

    aspect classes created

--- src/main/java/com/company/aspect/LoggingAspect.java
+++ src/main/java/com/company/aspect/LoggingAspect.java
@@ -1,9 +1,47 @@
 package com.company.aspect;
 
-import org.aspectj.lang.annotation.Aspect;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.keycloak.adapters.springsecurity.account.SimpleKeycloakAccount;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Slf4j //instead of line 12
 public class LoggingAspect {
+    private String getUsername() {
+        // get info who is login from keycloak
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        //keycloak is holding security info
+        SimpleKeycloakAccount userDetails = (SimpleKeycloakAccount) authentication.getDetails();
+        return userDetails.getKeycloakSecurityContext().getToken().getPreferredUsername();
+    }
+    //    Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+    @Pointcut("execution(* com.company.controller.ProjectController.*(..)) || execution(* com.company.controller.TaskController.*(..))")
+    public void anyProjectAndTaskControllerPC() {}
+
+    @Before("anyProjectAndTaskControllerPC()")
+    public void beforeAnyProjectAndTaskControllerAdvice(JoinPoint joinPoint) {
+        log.info("Before -> Method: {}, User: {}"
+                , joinPoint.getSignature().toShortString()
+                , getUsername());
+    }
+    @AfterReturning(pointcut = "anyProjectAndTaskControllerPC()", returning = "results")
+    public void afterReturningAnyProjectAndTaskControllerAdvice(JoinPoint joinPoint, Object results) {
+        log.info("After Returning -> Method: {}, User: {}, Results: {}"
+                , joinPoint.getSignature().toShortString()
+                , getUsername()
+                , results.toString());
+    }
+
+    @AfterThrowing(pointcut = "anyProjectAndTaskControllerPC()", throwing = "exception")
+    public void afterReturningAnyProjectAndTaskControllerAdvice(JoinPoint joinPoint, Exception exception) {
+        log.info("After Returning -> Method: {}, User: {}, Results: {}"
+                , joinPoint.getSignature().toShortString()
+                , getUsername()
+                , exception.getMessage());
+    }
 }
