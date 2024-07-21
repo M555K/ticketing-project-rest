dd9e4af29e3e03397d5b9910c28b2d12d5162b94
Maria <kitmaria.2015@gmail.com>
2024-07-21 14:00:27 -0500

    change return type

--- src/main/java/com/company/service/UserService.java
+++ src/main/java/com/company/service/UserService.java
@@ -10,7 +10,7 @@ import java.util.List;
 public interface UserService {
     List<UserDTO> listAllUsers();
     UserDTO findByUserName(String username);
-    void save(UserDTO user);
+    UserDTO save(UserDTO user);
     void deleteByUserName(String username);
     UserDTO update(UserDTO user);
     void delete(String username) throws TicketingProjectException;
